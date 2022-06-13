9d15b76f5c741064907539b24e000567110d1634
Matinho <matias68@gmail.com>
2022-06-13 19:02:14 -0300

    add pokemons dynamically

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,41 @@
 import type { GetStaticProps, NextPage } from 'next'
 import { pokeApi } from '../api'
 import { Layout } from '../components/layouts'
-import { PokemonListResponse } from '../interfaces'
+import { PokemonListResponse, SmallPokemon } from '../interfaces'
 
+interface PageProps {
+  pokemons: SmallPokemon[]
+}
 
-const Home: NextPage = ( props ) => {
-
-  console.log(props)
+const Home: NextPage<PageProps> = ({ pokemons }) => {
 
   return (
     <Layout title='Listado de Pokemons'>
       <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
+        { pokemons.map( (pokemon) => (
+            <li key={pokemon.id}>
+              #{ pokemon.id } - {pokemon.name}
+            </li>
+          ))
+        }
       </ul>
     </Layout>
   )
 }
 
-export const getStaticProps: GetStaticProps =async (ctx) => {
+export const getStaticProps: GetStaticProps = async (ctx) => {
   
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
 
+  const pokemons:SmallPokemon[] = data.results.map( (pokemon, index) => ({
+    ...pokemon,
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`
+  }))
+
   return {
     props: {
-      pokemons: data.results
+      pokemons
     }
   }
 }
