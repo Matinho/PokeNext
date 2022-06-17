2398cf91182ccfe7da7bef6cbe52426d7ed894d0
Matinho <matias68@gmail.com>
2022-06-17 03:28:30 -0300

    add pokemon info, navigation and favorites

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "../../components/layouts";
+
+export default function favorites() {
+  return (
+    <Layout title="Fokemons Favoritos">
+      <h1>Favoritos</h1>
+    </Layout>
+  )
+}
